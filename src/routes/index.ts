39a27d5e3c5f7beac4961e2402e6650c65968f7f
DeveloperDolon <dolonr718@gmail.com>
2026-02-27 02:10:42 +0600

    admin and user auth route connect to the main route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,8 @@
 import express from "express";
 
+import { AdminRoutes } from "#app/modules/admin/admin.routes.js";
+import { AuthRoutes } from "#app/modules/auth/auth.routes.js";
+
 const router = express.Router();
 
 const moduleRoutes = [
@@ -7,6 +10,14 @@ const moduleRoutes = [
     path: "/",
     route: router,
   },
+  {
+    path: "/auth",
+    route: AuthRoutes
+  }, 
+  {
+    path: "/admin",
+    route: AdminRoutes
+  }
 ];
 
 moduleRoutes.forEach((route) => router.use(route.path, route.route));
