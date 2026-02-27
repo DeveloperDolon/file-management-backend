8941ea6dba7473685565c16330ba107600de124a
DeveloperDolon <dolonr718@gmail.com>
2026-02-27 12:23:19 +0600

    backend api complete

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,6 +4,8 @@ import { AdminRoutes } from "#app/modules/admin/admin.routes.js";
 import { AuthRoutes } from "#app/modules/auth/auth.routes.js";
 import { PackageRoutes } from "#app/modules/package/package.routes.js";
 import { SubscriptionRoutes } from "#app/modules/subscription/subscription.routes.js";
+import { FolderRoutes } from "#app/modules/folder/folder.routes.js";
+import { FileRoutes } from "#app/modules/file/file.routes.js";
 
 const router = express.Router();
 
@@ -27,6 +29,14 @@ const moduleRoutes = [
   {
     path: "/subscriptions",
     route: SubscriptionRoutes
+  },
+  {
+    path: "/folders",
+    route: FolderRoutes
+  },
+  {
+    path: "/files",
+    route: FileRoutes
   }
 ];
 
